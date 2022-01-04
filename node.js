d6cad9efca5e7791b5f03074a66889e1fb9ad107
aolasuwang <aolasuwang@163.com>
2022-01-04 12:04:27 +0800

    qqq

--- node.js
+++ node.js
@@ -27,6 +27,7 @@ async function getDoc() {
             <!DOCTYPE html>
             <html>
                 <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover">
                     <title>mszd</title>
                     <meta charset="utf-8" />
                 </head>
